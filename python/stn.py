50720b85f9bdad001db9b46a04e9576d573f587a
Isaac Budzik <isaac.budzik@gmail.com>
2022-10-22 20:21:39 -0400

    20221022 - plans and explination of split complex numbers

---
+++ python/stn.py
@@ -0,0 +1,16 @@
+import math
+class stn:
+    def __init__(self,a,b,c,B):
+        self.a=a
+        self.b=b
+        self.c=c
+        self.B=B
+    def cp():
+        return stn(self.a,self,b,self.c,self.B)
+    def convert_basis():
+    def add():
+    def multiply():
+    def N():
+    def divide():
+    def exp():
+    def ln():
